e6976b66e9a90a962c6563858d9543562ee78cf8
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-02-01 18:11:19 -0600

    Feature: Added function to retrieve data
    The function uses fetch to get data from the API.

--- public/script.js
+++ public/script.js
@@ -17,6 +17,27 @@ function renderData(e) {
 }
 
 function submitQuery() {
+  fetch(`/api?q="${query.value}"&p=1&brand=${brand.value}`)
+    .then((response) => response.json())
+    .then((d) => {
+      let l={
+        hits:d.totalHits,
+        foods:d.foods.map(e=>{
+          return {
+            description: e.lowercaseDescription,
+            brand: e.brandOwner,
+            ingredients: e.ingredients,
+            servingSize: e.servingSize+e.servingSizeUnit,
+            nutrients: e.foodNutrients.map(e=>{
+              return e.nutrientName+": "+e.value+e.unitName
+            })
+          }
+        })
+      }
+      debug=d;
+      renderData(l);
+    });
+  console.log(query.value);
 }
 
 query.addEventListener("keyup", (e)=>{
