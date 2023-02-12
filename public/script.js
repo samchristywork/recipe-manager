55edb7bfdc63523d51a4250b6e08c92e15301197
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-02-11 19:18:42 -0600

    Feature: Add function to get a cookie

--- public/script.js
+++ public/script.js
@@ -21,6 +21,21 @@ let rdvs={
   "Total lipid (fat)": 75
 }
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 function clear_selection() {
   currentFoods=[];
   renderFoods();
