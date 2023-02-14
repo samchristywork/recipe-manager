17c5e0572d369e84f563c0f839c209a14f7f12a8
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-02-13 18:06:39 -0600

    Feature drop: Remove old api route

--- index.js
+++ index.js
@@ -50,51 +50,6 @@ app.get('/search', (req, res) => {
     });
   })
 
-app.get('/api', (req, res) => {
-  if (req.query.id!=null) {
-    fetch(`https://api.nal.usda.gov/fdc/v1/food/${req.query.id}?api_key=${process.env.API_KEY}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-    })
-      .then(e=>{
-        if (e.status==404) {
-          return;
-        }
-        return e.text()
-      })
-      .then(e=>{
-        res.send(e);
-      });
-  } else {
-    fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${process.env.API_KEY}`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "query": req.query.q,
-        "dataType": ["Branded", "Foundation"],
-        "pageNumber": req.query.p,
-        "pageSize": 25,
-        "brandOwner": req.query.brand
-      })
-    })
-      .then(e=>{
-        if (e.status==404) {
-          return;
-        }
-        return e.text();
-      })
-      .then(e=>{
-        res.send(e);
-      });
-  }
-})
-
 app.get('*', (req, res) => {
   res.status(400)
   res.send("Error");
